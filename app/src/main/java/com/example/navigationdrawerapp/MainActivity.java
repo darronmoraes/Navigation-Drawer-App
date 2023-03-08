ac2ac92da4c66c6ee5c52de7cc518d9dde3cebca
Darron Tacio Moraes <111967762+darronmoraes@users.noreply.github.com>
2023-03-09 02:02:58 +0530

    navigation drawer app initial commit

---
+++ app/src/main/java/com/example/navigationdrawerapp/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.navigationdrawerapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Variables
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Toolbar
+        toolbar = findViewById(R.id.toolbar_custom);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Drawer View
+        mDrawer = findViewById(R.id.myDrawerLayout);
+
+        // Drawer Navigation
+        nvDrawer = findViewById(R.id.nav_view);
+        setupDrawerContent(nvDrawer);    // Create setupDrawerContent() method
+    }
+
+    private void setupDrawerContent(NavigationView nvDrawer) {
+        nvDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectDrawerItem(item);   // Create SelectDrawerItem() method
+                return true;
+            }
+        });
+    }
+
+    private void selectDrawerItem(MenuItem item) {
+        Fragment fragment = null;
+        Class fragmentClass;
+
+        switch (item.getItemId()) {
+            case R.id.nav_first_element:
+                fragmentClass = LinkedInFragment.class;
+                break;
+            case R.id.nav_second_element:
+                fragmentClass = TelegramFragment.class;
+                break;
+            case R.id.nav_third_element:
+                fragmentClass = TwitterFragment.class;
+                break;
+            default:
+                fragmentClass = LinkedInFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (IllegalAccessException | InstantiationException e) {
+            throw new RuntimeException(e);
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fl_content, fragment).commit();
+
+        item.setChecked(true);
+        setTitle(item.getTitle());
+        mDrawer.closeDrawers();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
